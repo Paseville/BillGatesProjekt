633e62d2864f97329691f6b09ff49559c2c39546
Eric Mueller <emueller.de@gmail.com>
2022-02-23 18:17:23 +0100

    add Authentification

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser")
 const app = express();
 const mongoose = require("mongoose")
 
+const apiKey = process.env.API_KEY
 
 //initalise ejs
 app.set('view engine', 'ejs');
@@ -45,90 +46,114 @@ app.listen(3000, function() {
 
 //define post request on /create-new route
 app.post("/create-new", function(req, res) {
-  //get parameters from body of post request
-  const iNewTableNumber = req.body.tableNumber
-  const sNewRandomAuthKey = req.body.randomAuthKey
-  const arrNewBoughtItems = req.body.boughtItems
-  //create an Item object with postet parameters
-  const newBill = new Bill({
-    tableNumber: iNewTableNumber,
-    randomAuthKey: sNewRandomAuthKey,
-    boughtItems: arrNewBoughtItems,
-    //always initalised with 0 on new entrys
-    done: 0
-  })
-  //save new Bill document to Item collection
-  newBill.save(function(err) {
-    //errorhandling
-    if (err) {
-      console.log("Error while saving new element to Database (post to /create-new route ________________" + err)
-    } else {
-      console.log("Successfully added new entry to Database")
-      res.send("Added Entry")
-    }
-  })
+  //test if the user has the right key
+  if(req.query.auth === apiKey){
+    //get parameters from body of post request
+    const iNewTableNumber = req.body.tableNumber
+    const sNewRandomAuthKey = req.body.randomAuthKey
+    const arrNewBoughtItems = req.body.boughtItems
+    //create an Item object with postet parameters
+    const newBill = new Bill({
+      tableNumber: iNewTableNumber,
+      randomAuthKey: sNewRandomAuthKey,
+      boughtItems: arrNewBoughtItems,
+      //always initalised with 0 on new entrys
+      done: 0
+    })
+    //save new Bill document to Item collection
+    newBill.save(function(err) {
+      //errorhandling
+      if (err) {
+        console.log("Error while saving new element to Database (post to /create-new route ________________" + err)
+      } else {
+        console.log("Successfully added new entry to Database")
+        res.send("Added Entry")
+      }
+    })
+  } else{
+    res.send("Access Denied wrong Key")
+  }
+
 })
 
 //get route for getting the 5 newest elements with a done status of 0
 app.get("/get", function(req, res) {
-  //search for Bills within database with attribute done = 0
-  Bill.find({
-    done: 0
-  }, function(err, foundBills) {
-    if (err) {
-      console.log(err)
-      res.send("an error has occurd on /get route")
-    } else {
-      //when database holds less than five documents for loop will throw an arror
-      if (foundBills.length < 5) {
-        console.log("less than five entrys in database found")
-        res.send(foundBills)
+  //Test if user has the right key
+  if(req.query.auth === apiKey){
+    //search for Bills within database with attribute done = 0
+    Bill.find({
+      done: 0
+    }, function(err, foundBills) {
+      if (err) {
+        console.log(err)
+        res.send("an error has occurd on /get route")
       } else {
-        //new elements are added at the end of database so start for loop at end of array
-        //                                    JUST 5 OF THE ELEMENTS
-        for (i = (foundBills.length - 1); i >= (foundBills.length - 6); i--) {
-          console.log("At least five database entrys found")
-          var newBills = []
-          //add five newest found elements to array to send
-          newElements.push(foundBills[i])
-          res.send(newBills)
+        //when database holds less than five documents for loop will throw an arror
+        if (foundBills.length < 5) {
+          console.log("less than five entrys in database found")
+          res.send(foundBills)
+        } else {
+          //new elements are added at the end of database so start for loop at end of array
+          //                                    JUST 5 OF THE ELEMENTS
+          for (i = (foundBills.length - 1); i >= (foundBills.length - 6); i--) {
+            console.log("At least five database entrys found")
+            var newBills = []
+            //add five newest found elements to array to send
+            newElements.push(foundBills[i])
+            res.send(newBills)
+          }
         }
-      }
 
-    }
-  })
+      }
+    })
+  } else {
+    res.send("Access Denied wrong key")
+  }
+
 })
 
 //get route for getting all elements which are not yet done
 app.get("/get-all", function(req, res){
-  //search for Bills within database with attribute done = 0
-  Bill.find({done: 0}, function(err, foundBills){
-    //error handling
-    if (err){
-      console.log("an error has occured in the /get-all route")
-      res.send("an error has occurd on the get all rout")
-    } else {
-      console.log("all Bills have been sent successfully")
-      res.send(foundBills)
-    }
-  })
+  //Test if user has the right key
+  if (req.query.auth === apiKey){
+    //search for Bills within database with attribute done = 0
+    Bill.find({done: 0}, function(err, foundBills){
+      //error handling
+      if (err){
+        console.log("an error has occured in the /get-all route")
+        res.send("an error has occurd on the get all rout")
+      } else {
+        console.log("all Bills have been sent successfully")
+        res.send(foundBills)
+      }
+    })
+  } else {
+    res.send("Access Denied wrong key")
+  }
+
 })
 
 
 //Update route for Updating done to 0 of item with itemID
 app.get("/update/:billID", function(req, res){
-  const billID = req.params.billID
-  Bill.findById(billID, function(err, bill){
-    if (err){
-      res.send(err)
-      console.log("an error has occured while searching for requested id")
-    } else{
-      //udate daone Status to 1
-      bill.done = 1,
-      console.log("successfully update item")
-    }
-    bill.save()
-    res.send("updated successfully")
-  })
+  //Test if user has the right key
+  if (req.query.auth === apiKey){
+    const billID = req.params.billID
+    Bill.findById(billID, function(err, bill){
+      if (err){
+        res.send(err)
+        console.log("an error has occured while searching for requested id")
+      } else{
+        //udate daone Status to 1
+        bill.done = 1,
+        console.log("successfully update item")
+      }
+      bill.save()
+      res.send("updated successfully")
+    })
+
+  } else{
+    res.send("Acess Denied wrong key")
+  }
 
 })
